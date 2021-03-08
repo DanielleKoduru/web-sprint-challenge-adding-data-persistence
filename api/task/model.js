3ae14227863ca83c791e6673d2fa5e235ebe1d5a
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-03-07 19:42:44 -0700

    danielle-koduru

--- api/task/model.js
+++ api/task/model.js
@@ -1,6 +1,17 @@
 // build your `Task` model here
 const db = require("../../data/dbConfig")
 
+const getTasks = async () => {
+    const tasks = await db('tasks')
+        .select("*")
+    return tasks.map(task => {
+        return {
+            ...task,
+            task_completed: boolean(task.task_completed)
+        }
+    })
+}
+
 const boolean = (num) => {
     if (num === 0 || null) {
         return false
@@ -38,26 +49,26 @@ const newTask = async (task) => {
     }
 }
 
-const getTasks = async () => {
-    const tasks = await db('tasks as t')
-        .join("projects as p", "t.project_id", "p.project_id")
-        .select(
-            "t.task_id",
-            "t.task_description",
-            "t.task_notes",
-            "t.task_completed",
-            "p.project_name",
-            "p.project_description"
-        )
-    return tasks.map(task => {
-        if (task.task_completed === 1) {
-            task.task_completed === true
-        } else {
-            task.task_completed = false
-        }
-        return tasks
-    })
-}
+// const getTasks = async () => {
+//     const tasks = await db('tasks as t')
+//         .join("projects as p", "t.project_id", "p.project_id")
+//         .select(
+//             "t.task_id",
+//             "t.task_description",
+//             "t.task_notes",
+//             "t.task_completed",
+//             "p.project_name",
+//             "p.project_description"
+//         )
+//     return tasks.map(task => {
+//         if (task.task_completed === 1) {
+//             task.task_completed === true
+//         } else {
+//             task.task_completed = false
+//         }
+//         return tasks
+//     })
+// }
 
 module.exports = {
     getTasks,
